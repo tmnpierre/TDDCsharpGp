2b7c3ad45387e7a3449da79a9284ce45f4aa0dd0
tmnpierre <tomyn.pierre@gmail.com>
2024-01-26 13:40:44 +0100

    RED: WhenDictionary_IsEmpty_Then_CountIsZero

---
+++ DictionaryReplacer.Core/Dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DictionaryReplacer.Core
+{
+    public class Dictionary
+    {
+        public static string ReplaceString(string input, Dictionary<string, string> replacements)
+        {
+            return input;
+        }
+    }
+}
---
+++ DictionaryReplacer.Tests/DictionaryTests.cs
@@ -0,0 +1,23 @@
+﻿using DictionaryReplacer.Core;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DictionaryReplacer.Tests
+{
+    [TestFixture]
+    public class StringReplacerTests
+    {
+        [Test]
+        public void WhenDictionary_IsEmpty_Then_CountIsZero()
+        {
+            var dictionary = new Dictionary<string, string>();
+
+            var result = dictionary.Count;
+
+            Assert.AreEqual(0, result);
+        }
+    }
+}
