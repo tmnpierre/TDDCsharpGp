e90f9e98ce3ea5212b9cfba1655c1cc289b21c3d
tmnpierre <tomyn.pierre@gmail.com>
2024-01-26 13:56:33 +0100

    RED: WhenSingleReplacementInString_Then_ReplaceCorrectly

--- DictionaryReplacer.Tests/DictionaryTests.cs
+++ DictionaryReplacer.Tests/DictionaryTests.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
 namespace DictionaryReplacer.Tests
 {
     [TestFixture]
-    public class StringReplacerTests
+    public class DictionaryTests
     {
         [Test]
         public void WhenDictionary_IsEmpty_Then_CountIsZero()
@@ -19,5 +19,15 @@ namespace DictionaryReplacer.Tests
 
             Assert.AreEqual(0, result);
         }
+
+        [Test]
+        public void WhenSingleReplacementInString_Then_ReplaceCorrectly()
+        {
+            var replacements = new Dictionary<string, string> { { "temp", "temporary" } };
+            var result = Dictionary.ReplaceString("$temp$", replacements);
+
+            Assert.AreEqual("temporary", result);
+        }
+
     }
 }
